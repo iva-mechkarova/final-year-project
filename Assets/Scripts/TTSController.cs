f210b627aff03e068f391f0b20a2356b011a60b4
Iva Mechkarova <iva.mechkarova@ucdconnect.ie>
2022-02-10 15:47:57 +0000

    Removed EasyTTS Plugin, changed TTS to built in Android Speech Plugin

--- Assets/Scripts/TTSController.cs
+++ Assets/Scripts/TTSController.cs
@@ -3,25 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using TextSpeech;
 
-public class SpeechController : MonoBehaviour
+public class TTSController : MonoBehaviour
 {
     const string LANG_CODE = "en-US";
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         Setup(LANG_CODE);
     }
 
     #region Text to Speech
 
-    public void StartSpeaking(string message) {
-        TextToSpeech.instance.StartSpeak(message);
+    public void StartSpeaking(string wordToSpeak) {
+        TextToSpeech.instance.StartSpeak(wordToSpeak);
     }
 
     #endregion
 
-    void Setup(string code) {
+    private void Setup(string code) {
         TextToSpeech.instance.Setting(code, 1, 1);
     }
 }
