8bcd5bd882fb2d6b81ddf91a53de65da886e4152
Iva Mechkarova <iva.mechkarova@ucdconnect.ie>
2022-03-17 04:12:01 +0000

    Calculate/store phonetic distance using soundex

--- Assets/Scripts/SendSpellingToDB.cs
+++ Assets/Scripts/SendSpellingToDB.cs
@@ -11,6 +11,7 @@ public class SendSpellingToDB : MonoBehaviour {
     protected string targetWord;
     protected int repeatCount = 0; // Count how many times repeat btn is pressed
     protected int selectedWordList = 0; // Stores which list we are reading from (asc difficulty of 0 - 4)
+    protected int phoneticDistance = 0; // Stores the most recent attempt's distance from target word (0 - 4, where 0 is identical)
 
     protected IEnumerator RecordAskedWord() {
         WWWForm form = new WWWForm();
@@ -67,7 +68,8 @@ public class SendSpellingToDB : MonoBehaviour {
     protected IEnumerator RecordAttempt(string attempt) {
         WWWForm form = new WWWForm();
         // Send the necessary attributes to the API
-        form.AddField("id", Guid.NewGuid().ToString());
+        string attemptId = Guid.NewGuid().ToString();
+        form.AddField("id", attemptId);
         form.AddField("wordId", id);
         form.AddField("attempt", attempt);
 
@@ -75,10 +77,46 @@ public class SendSpellingToDB : MonoBehaviour {
         using (UnityWebRequest www = UnityWebRequest.Post("http://192.168.0.122/sounditout/RecordAttempt.php", form)) {
             yield return www.SendWebRequest();
 
+            string[] pages = "http://192.168.0.122/sounditout/RecordAttempt.php".Split('/');
+            int page = pages.Length - 1;
+
+            switch (www.result) {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + www.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + www.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + www.downloadHandler.text);
+
+                    string[] soundexCodes = www.downloadHandler.text.Split(' ');
+                    phoneticDistance = 0;
+                    for (int i=0; i<4; i++) {
+                        if (soundexCodes[0][i]!=soundexCodes[1][i])
+                            phoneticDistance++;
+                    }
+                    StartCoroutine(UpdatePhoneticDistance(attemptId));
+                    break;
+            }
+        }
+    }
+
+    protected IEnumerator UpdatePhoneticDistance(string attemptId) {
+        WWWForm form = new WWWForm();
+        // Send the necessary attributes to the API
+        form.AddField("id", attemptId);
+        form.AddField("phoneticDistance", phoneticDistance);
+
+        // Call the Web API that updates the skipped value to TRUE in the DB for the given id
+        using (UnityWebRequest www = UnityWebRequest.Post("http://192.168.0.122/sounditout/UpdatePhoneticDistance.php", form)) {
+            yield return www.SendWebRequest();
+
             if (www.result != UnityWebRequest.Result.Success)
                 Debug.Log(www.error);
             else
-                Debug.Log("Attempt recorded successfully!");
+                Debug.Log("Phonetic distance updated successfully!");
         }
     }
 }
--- ServerAPIs/RecordAttempt.php
+++ ServerAPIs/RecordAttempt.php
@@ -20,8 +20,18 @@ if ($conn->connect_error) {
 $sql = "INSERT INTO attempts(id, word_id, attempt) VALUES ('" . $id. "', '" . $wordId. "', '" . $attempt. "')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New attempt inserted successfully";
-
+  //echo "New attempt inserted successfully";
+  $sql = "SELECT soundex_code FROM asked_words WHERE id='" . $wordId. "'";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo $row["soundex_code"]. " " . soundex($attempt);
+      break;
+    }
+    } else {
+      echo "0 results";
+    }
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
