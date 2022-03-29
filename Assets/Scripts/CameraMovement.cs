683e60bfc2e875bcfa1e89efb0d8222455d7af58
Iva Mechkarova <iva.mechkarova@ucdconnect.ie>
2022-03-29 02:27:11 +0100

    Added additional comments

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -9,6 +9,7 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Move camera on x-axis at specified speed
         transform.position += new Vector3(cameraSpeed * Time.deltaTime, 0, 0);
     }
 }
--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -6,10 +6,10 @@ using UnityEngine.UI;
 public class CharacterSelection : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] characters;
+    private GameObject[] characters; // Array of character images
 
     [SerializeField]
-    private GameObject[] costs;
+    private GameObject[] costs; // Array of costs of each character
 
     private int currCharacterIndex;
     private int playerScore;
@@ -21,23 +21,29 @@ public class CharacterSelection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currCharacterIndex = 0;
+        currCharacterIndex = 0; // Show first character
+        // Get accumulated score or if player hasn't played yet set score to 0
         playerScore = PlayerPrefs.HasKey("totalScore") ? PlayerPrefs.GetInt("totalScore") : 0;
-        CheckStatusOfCharacter(0);
+        CheckStatusOfCharacter(0); // Check if character is unlocked, locked or selected
     }
 
+    // Show character to the right of current one
     public void RightBtnPressed() {
+        // If curr indx = num characters-1 then we have reached last character so show character at indx 0
         currCharacterIndex = currCharacterIndex < characters.Length-1 ? currCharacterIndex+1 : 0;
         NextCharacter();
     }
 
+    // Show character to the left of current one
     public void LeftBtnPressed() {
+        // If curr indx = 0 then we have reached first character so show last character
         currCharacterIndex = currCharacterIndex > 0 ? currCharacterIndex-1 : characters.Length-1;
         NextCharacter();
     }
 
     private void NextCharacter() {   
         for (int i=0; i<characters.Length; i++) {
+            // Deactivate all characters that aren't selected
             if (i != currCharacterIndex) {
                 characters[i].SetActive(false);
                 costs[i].SetActive(false);
@@ -48,42 +54,49 @@ public class CharacterSelection : MonoBehaviour
         }
     }
 
+    // Method to switch character i.e. character that is used in the game
     public void SelectButtonPressed() {
         PlayerPrefs.SetInt("selectedCharacter", currCharacterIndex);
         CheckStatusOfCharacter(currCharacterIndex);
     }
 
+    // Method to unlock a character
     public void UnlockButtonPressed() {
+        // Store that the character has been unlocked
         PlayerPrefs.SetInt("fish-"+currCharacterIndex.ToString(), 1);
+        // Subtract cost from total score and save new total
         playerScore -= GetCurrCharacterCost();
         PlayerPrefs.SetInt("totalScore", playerScore);
         CheckStatusOfCharacter(currCharacterIndex);
     }
 
+    // Method to update what is shown for the current character
     private void CheckStatusOfCharacter(int i) {
-        selectedText.SetActive(false);
-        characters[i].SetActive(true);
-        selectButton.gameObject.SetActive(false);
-        costs[0].SetActive(false);
+        selectedText.SetActive(false); // Deactivate selected text
+        characters[i].SetActive(true); // Show current character
+        selectButton.gameObject.SetActive(false); // Deactivate select btn
+        // Check if character is unlocked by checking if it is saved in PlayerPrefs
         int isUnlocked = PlayerPrefs.HasKey("fish-"+i.ToString()) ? PlayerPrefs.GetInt("fish-"+i.ToString()) : 0;
+
+        // If curr player is unlocked
         if (isUnlocked == 1) {
-            costs[i].SetActive(false);
-            unlockButton.gameObject.SetActive(false);
-            if (PlayerPrefs.GetInt("selectedCharacter") == i) {
+            costs[i].SetActive(false); // Don't display cost as unlocked already
+            unlockButton.gameObject.SetActive(false); // Diactivate unlock btn
+
+            // Check if this is the currently selected character
+            if (PlayerPrefs.GetInt("selectedCharacter") == i)
                 selectedText.SetActive(true);
-            }
-            else {
+            else
                 selectButton.gameObject.SetActive(true);
-                costs[0].SetActive(true);
-            }
         }
         else {
             CanAffordCharacter();
             unlockButton.gameObject.SetActive(true);
-            costs[i].SetActive(true);
+            costs[i].SetActive(true); // Display cost of character as not unlocked
         }
     }
 
+    // Method to check if player has enough accumulated stars to unlock a character
     private void CanAffordCharacter() {
         int cost = GetCurrCharacterCost();
         if (playerScore >= cost) {
@@ -94,6 +107,7 @@ public class CharacterSelection : MonoBehaviour
         }
     }
 
+    // Method to get the cost from the curr character's cost text
     private int GetCurrCharacterCost() {
         string costText = costs[currCharacterIndex].transform.GetChild(0).gameObject.GetComponent<Text>().text;
         return int.Parse(costText);
